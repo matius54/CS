523c9ad10e1f171104947e0f8f20b88ffedcfdbc
matius54 <mat54200454@gmail.com>
2024-03-22 19:01:59 -0400

    añadida pagina simple usando css, separada en componentes para facilitar la reutilizacion.

---
+++ components/header.php
@@ -0,0 +1,12 @@
+<?php
+    if(!isset($title)) $title = "titulo";
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?=$title?></title>
+    <link href="components/cs.css" rel="stylesheet">
+</head>
+<body>
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php include "components/header.php";?>
+<header>
+    <h1><?= $title ?></h1>
+    <a href="#" class="button">opcion 1</a>
+    <a href="#" class="button">opcion 2</a>
+    <a href="#" class="button">opcion 3</a>
+</header>
+<main></main>
+<?php include "components/footer.php";?>
