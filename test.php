b5517e64b7860d03bf60f2ac9f4b83038b0e6182
matius54 <mat54200454@gmail.com>
2024-03-24 14:28:01 -0400

    actualizadas las pruebas

--- test.php
+++ test.php
@@ -1,10 +1,27 @@
 <?php
-    require_once "./db.php";
-    //require_once "./utils.php";
+    include "components/header.php";
+    require_once "utils.php";
+    /*
+    require_once "db.php";
     $cn = DB::getInstance();
-    //$cn->execute("INSERT INTO tabla (name, description) VALUES (?,?)",["hola","xd"]);
-    $cn->execute("SELECT * FROM tabla");
+    $cn->execute("SELECT * FROM categoria");
     var_dump($cn->rowCount());
     $result = $cn->fetch();
     var_dump($result);
+    */
+
+    require_once "ve.php";
+    $ve = new Ve;
+    $states = $ve->states();
+    echo HTML::array2list($states, "states", "selecciona el estado", sameValueName: true);
+    $cities = $ve->cities();
+    echo HTML::array2list($cities, "cities", "selecciona tu ciudad", sameValueName: true);
+    
 ?>
+<script src="ajax.js"></script>
+<script src="ve.js"></script>
+<script src="list.js"></script>
+<script>
+    const ve = new Ve(document.querySelector("select[name=\"states\"]"),document.body);
+</script>
+<?php include "components/footer.php" ?>
