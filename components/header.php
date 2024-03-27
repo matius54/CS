e7f4eb583d0e4e38c94274e0fda21a964678580e
Hechemama <jefagoes@gmail.com>
2024-03-27 12:20:29 -0400

    agregue enlaces de bootstrap

--- components/header.php
+++ components/header.php
@@ -1,12 +1,16 @@
 <?php
-    if(!isset($title)) $title = "titulo";
+if (!isset($title)) $title = "titulo";
 ?>
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?=$title?></title>
+    <title><?= $title ?></title>
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <script src="bootstrap/js/bootstrap.bundle.min.js"></script>
     <link href="components/cs.css" rel="stylesheet">
 </head>
+
 <body>
