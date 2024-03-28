72b2dbd6838942a6cbfa1ba2485e9a609ae864c5
Hechemama <jefagoes@gmail.com>
2024-03-27 22:16:09 -0400

    Sigo sin hacerle nada, no se que le pasa

--- index.php
+++ index.php
@@ -1,12 +1,14 @@
 <?php include "components/header.php"; ?>
-
 <header>
+    <img src="components/images.webp" alt="Logo">
+</header>
+<nav>
     <h1><?= $title ?></h1>
     <a href="#" class="button">opcion 1</a>
     <a href="#" class="button">opcion 2</a>
     <a href="#" class="button">opcion 3</a>
-</header>
+</nav>
 <main>
-
+    
 </main>
 <?php include "components/footer.php"; ?>
