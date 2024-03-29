7e17f2ffdbe625f8b0aa8831f8363d910e26740f
matius54 <mat54200454@gmail.com>
2024-03-28 23:15:54 -0400

    formulario modificado

--- formulario.php
+++ formulario.php
@@ -1,22 +1,22 @@
 <?php
-include "components/header.php";
-include "index.php";
+    $title = "Formulario";
+    include "components/header.php";
 ?>
 
-<form action="" method="post" class="container d-grid text-align-center">
+<form action="php/profesor.php?action=setdata" method="post" class="container d-grid text-align-center">
     <div class="row">
         <fieldset class="col">
             <legend>Datos Personales</legend>
             <div class="form-floating mb-3">
-                <input type="text" class="form-control" name="nombre" id="nombre" placeholder="" />
+                <input type="text" class="form-control" name="firstname" id="nombre" placeholder="" />
                 <label for="nombre">Nombres</label>
             </div>
             <div class="form-floating mb-3">
-                <input type="text" class="form-control" name="apellido" id="apellido" placeholder="" />
+                <input type="text" class="form-control" name="lastname" id="apellido" placeholder="" />
                 <label for="apellido">Apellidos</label>
             </div>
             <div class="form-floating mb-3">
-                <input type="text" class="form-control" name="cedula" id="cedula" placeholder="" />
+                <input type="text" class="form-control" name="ci" id="cedula" placeholder="" />
                 <label for="cedula">Cedula</label>
             </div>
             <div class="form-floating mb-3">
@@ -24,23 +24,32 @@ include "index.php";
                 <label for="rif">Rif</label>
             </div>
             <div class="form-floating mb-3">
-                <input type="date" class="form-control" name="nacimiento" id="fecha" placeholder="1994" />
+                <input type="date" class="form-control" name="birthdate" id="fecha" placeholder="1994" />
                 <label for="fecha">Fecha de Nacimiento</label>
             </div>
         </fieldset>
         <fieldset class="col">
             <legend>Contacto</legend>
             <div class="form-floating mb-3">
-                <input type="text" class="form-control" name="nombre" id="telefono" placeholder="" />
+                <input type="text" class="form-control" name="phone" id="telefono" placeholder="" />
                 <label for="telefono">Telefono</label>
             </div>
             <div class="form-floating mb-3">
-                <input type="text" class="form-control" name="apellido" id="correo" placeholder="" />
+                <input type="text" class="form-control" name="email" id="correo" placeholder="" />
                 <label for="correo">Correo</label>
             </div>
             <div class="form-floating mb-3">
-                <input type="text" class="form-control" name="nombre" id="direccion" placeholder="" />
+                <input type="text" class="form-control" name="address" id="direccion" placeholder="" />
                 <label for="direccion">Direccion</label>
+            </div>
+
+            <div class="form-floating mb-3">
+                <input type="text" class="form-control" name="state" id="estado" placeholder="" />
+                <label for="estado">Estado</label>
+            </div>
+            <div class="form-floating mb-3">
+                <input type="text" class="form-control" name="city" id="ciudad" placeholder="" />
+                <label for="ciudad">Ciudad</label>
             </div>
         </fieldset>
     </div>
@@ -48,3 +57,7 @@ include "index.php";
         Guardar Datos
     </button>
 </form>
+
+<?php
+    include "components/footer.php";
+?>
