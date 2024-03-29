17d06a8e148f1fc6400e9e41bdf1fba08b2eb473
matius54 <mat54200454@gmail.com>
2024-03-28 23:18:31 -0400

    formulario de login modificado

--- login.php
+++ login.php
@@ -1,19 +1,19 @@
 <?php
-
+$title = "Iniciar Sesión";
 include "components/header.php";
 ?>
 
-<form action="" method="post" style="width: 100%; margin-top: 1rem;">
+<form action="php/profesor.php?action=login" method="post" style="width: 100%; margin-top: 1rem;">
     <fieldset class="container m-auto p-5 d-grid gap-3 shadow">
         <legend class="h2 mb-5">Inicio de Sesion</legend>
         <div class="row">
             <div class="col d-flex flex-column gap-5 align-items-center p-5">
                 <div class="form-floating w-75 mr-5">
-                    <input type="text" class="form-control" name="usuario" id="usuario" placeholder="" />
+                    <input type="text" class="form-control" name="username" id="usuario" placeholder="" />
                     <label for="usuario">Usuario</label>
                 </div>
                 <div class="form-floating mb-3 w-75">
-                    <input type="password" class="form-control" name="clave" id="clave" placeholder="" />
+                    <input type="password" class="form-control" name="password" id="clave" placeholder="" />
                     <label for="clave">Contraseña</label>
                 </div>
             </div>
@@ -24,3 +24,5 @@ include "components/header.php";
         <button type="submit" class="btn m-auto w-25">Iniciar Sesión</button>
     </fieldset>
 </form>
+
+<?php include "components/footer.php"; ?>
