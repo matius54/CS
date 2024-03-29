c4f244372f4bf9904094f34ef6557550d460910e
matius54 <mat54200454@gmail.com>
2024-03-28 23:16:18 -0400

    elementos del index movidos a header.php

--- index.php
+++ index.php
@@ -1,14 +1,3 @@
 <?php include "components/header.php"; ?>
-<header>
-    <img src="components/images.webp" alt="Logo">
-</header>
-<nav>
-    <h1><?= $title ?></h1>
-    <a href="#" class="button">opcion 1</a>
-    <a href="#" class="button">opcion 2</a>
-    <a href="#" class="button">opcion 3</a>
-</nav>
-<main>
     
-</main>
 <?php include "components/footer.php"; ?>
