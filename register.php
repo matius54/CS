1fdd74b550a1de156c034ce478958aa86ca5e1d8
matius54 <mat54200454@gmail.com>
2024-03-28 23:19:14 -0400

    creado formulario de registro provisional

---
+++ register.php
@@ -0,0 +1,7 @@
+<?php include "components/header.php" ?>
+<form action="php/profesor.php?action=register" method="post">
+    <input type="text" name="username" placeholder="usuario">
+    <input type="password" name="password" placeholder="contraseña">
+    <input type="submit" value="Registrar">
+</form>
+<?php include "components/footer.php" ?>
