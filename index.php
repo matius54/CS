11d281a1fc5162262586a962a279e34a249580b6
matius54 <mat54200454@gmail.com>
2024-03-30 19:41:52 -0400

    añadi titulo y un boton para ver los profesores al index

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
-<?php include "components/header.php"; ?>
-    
+<?php
+    $title = "Página principal";
+    include "components/header.php";
+?>
+    <div>
+        <a class="button btn" href="php/profesor.php?action=paginate">Ver profesores</a>
+    </div>
 <?php include "components/footer.php"; ?>
